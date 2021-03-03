fe3e0a556bb47329ebc471a406e21bb8c018c795
ChopperCP <568624486@qq.com>
2021-03-03 12:08:13 +0800

    Implemented the -e feature

--- NC(Netcat v2)/nc.py
+++ NC(Netcat v2)/nc.py
@@ -20,8 +20,8 @@ def usage():
     print("Usage: nc.py -t target_host -p port")
     print(
         "-l --listen                - listen on [host]:[port] for incoming connections")
-    print("-e --execute=file_to_run   - execute a Python file upon receiving a connection (Option required for both ends)")
-    print("-c --command               - initialize a Python interactive shell to connection (Option required for both ends)")
+    print("-e --execute=file_to_run   - execute a Python file upon receiving a connection")
+    print("-c --command               - initialize a Python interactive shell to connection")
     print(
         "-u --upload=destination    - upon receiving connection upload a file and write to [destination]")
     print("Examples: ")
@@ -65,27 +65,28 @@ for o, a in opts:
 
 # connect to victim
 if not listen and len(target) and port > 0:
+    # read in the buffer from the commandline
+    # this will block, so send CTRL-D if not sending input
+    # to stdin
+    # buffer = sys.stdin.read()
+    # buffer = buffer.encode('utf8')
+
+    # establish a socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((target, port))
+    print("[*]Successfully connected to {}:{}".format(target, port))
+
+    # open a new thread for receving data
+    def recv_n_print():
+        while True:
+            data = s.recv(4096)
+            data = data.decode('utf8')
+            print(data)
+    t = threading.Thread(target=recv_n_print)
+    t.start()
+
     if command:
-        # read in the buffer from the commandline
-        # this will block, so send CTRL-D if not sending input
-        # to stdin
-        # buffer = sys.stdin.read()
-        # buffer = buffer.encode('utf8')
-
-        # establish a socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
-        print("[*]Successfully connected to {}:{}".format(target, port))
-
-        # open a new thread for receving data
-        def recv_n_print():
-            while True:
-                data = s.recv(4096)
-                data = data.decode('utf8')
-                print(data)
-        t = threading.Thread(target=recv_n_print)
-        t.start()
-
+        s.send('c'.encode('utf8'))  # tell server mode:command
         while True:
             # input command
             cmd = input(">>>")
@@ -96,8 +97,17 @@ if not listen and len(target) and port > 0:
             # send data off
             s.send(cmd)
 
+    if len(execute):
+        s.send('e'.encode('utf8'))  # tell server mode:execute
+        # send content of the file
+        data = open(execute).read()
+        s.send(data.encode('utf8'))
+
 
 def server_loop():
+    global command
+    global execute
+
     server_socket = socket.socket(
         socket.AF_INET, socket.SOCK_STREAM)  # the socket of server
     server_socket.bind((target, port))
@@ -106,6 +116,27 @@ def server_loop():
 
     # handle clients
     def handle_client(client_socket):
+        global command
+        global execute
+
+        def encode_n_send(msg):
+            client_socket.send(msg.encode('utf8'))
+
+        # get the mode operating on command/execute...
+        print("[*]Getting the operation mode: ", end='')
+        while True:
+            cmd = client_socket.recv(1)
+            cmd = cmd.decode('utf8')
+            if cmd == 'c':
+                print("Command")
+                command = True
+                break
+            elif cmd == 'e':
+                print("Execute")
+                execute = 'True'  # we don't know the file path. It's going to be sent by client
+                break             # just to trgger the 'if' statement
+
+        # execute Python command one at a time using eval()
         if command:
             while True:
                 cmd = client_socket.recv(4096)  # receive data
@@ -117,12 +148,21 @@ def server_loop():
                         eval(cmd)
                         sys.stdout = old_stdout
                         result = mystdout.getvalue()    # get the result of eval()
-                        result = result.encode('utf8')
-                        # print(result)
-                        client_socket.send(result)      # send off result
+                        encode_n_send(result)
+
                     except Exception as e:
-                        client_socket.send(str(e).encode('utf8'))
-        # DOTO
+                        encode_n_send(str(e))
+
+        # read and execute an entire Python file
+        if len(execute):
+            data = client_socket.recv(40960)
+            old_stdout = sys.stdout
+            sys.stdout = mystdout = StringIO()
+            result = exec(data.decode('utf8'))
+            sys.stdout = old_stdout
+            result = mystdout.getvalue()    # get the result of eval()
+            encode_n_send(result)
+            encode_n_send("[*]Script successfully executed!")
 
     while True:
         client_socket, client_addr = server_socket.accept()
